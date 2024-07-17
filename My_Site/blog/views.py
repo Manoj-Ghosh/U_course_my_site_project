20417eff7868095df6bdf0c91963fe4b3f58b737
Manoj-Ghosh <manojghoshdk@gmail.com>
2024-07-17 20:30:55 +0530

    multiple featured added in views.py

--- My_Site/blog/views.py
+++ My_Site/blog/views.py
@@ -1,4 +1,110 @@
+from datetime import date
+
 from django.shortcuts import render
+
+
+posts = [
+    {
+        "slug":"hike-in-the-mountain",
+        "image": "mountains.jpeg",
+        "author": "Manoj",
+        "date": date(2024, 7, 17),
+        "title":"Mountain Hiking",
+        "excerpt": "There's is nothing like views you get when hiking in the mountains!I wasn't even prepared for what happend whilst I was enjoying the view!",
+        "content": """
+            As of June 2020, the Bengali Wikipedia is the only online free 
+            encyclopedia written in the Bengali language. It is also one of 
+            the largest Bengali content related sites on the internet. 
+            The mobile version of the Bengali Wikipedia was launched in 2010
+             As of June 2020, the Bengali Wikipedia is the only online free 
+            encyclopedia written in the Bengali language. It is also one of 
+            the largest Bengali content related sites on the internet. 
+            The mobile version of the Bengali Wikipedia was launched in 2010
+             
+              As of June 2020, the Bengali Wikipedia is the only online free 
+            encyclopedia written in the Bengali language. It is also one of 
+            the largest Bengali content related sites on the internet. 
+            The mobile version of the Bengali Wikipedia was launched in 2010
+             As of June 2020, the Bengali Wikipedia is the only online free 
+            encyclopedia written in the Bengali language. It is also one of 
+            the largest Bengali content related sites on the internet. 
+            The mobile version of the Bengali Wikipedia was launched in 2010
+             
+              As of June 2020, the Bengali Wikipedia is the only online free 
+            encyclopedia written in the Bengali language. It is also one of 
+            the largest Bengali content related sites on the internet. 
+            The mobile version of the Bengali Wikipedia was launched in 2010 """
+    },
+        
+    
+
+    {
+        "slug":"hike-in-the-mountain",
+        "image": "mountains.jpeg",
+        "author": "Manoj",
+        "date": date(2023, 8, 17),
+        "title":"Mountain Hiking",
+        "excerpt": "There's is nothing like views you get when hiking in the mountains!I wasn't even prepared for what happend whilst I was enjoying the view!",
+        "content": """
+            As of June 2020, the Bengali Wikipedia is the only online free 
+            encyclopedia written in the Bengali language. It is also one of 
+            the largest Bengali content related sites on the internet. 
+            The mobile version of the Bengali Wikipedia was launched in 2010
+             As of June 2020, the Bengali Wikipedia is the only online free 
+            encyclopedia written in the Bengali language. It is also one of 
+            the largest Bengali content related sites on the internet. 
+            The mobile version of the Bengali Wikipedia was launched in 2010
+             
+              As of June 2020, the Bengali Wikipedia is the only online free 
+            encyclopedia written in the Bengali language. It is also one of 
+            the largest Bengali content related sites on the internet. 
+            The mobile version of the Bengali Wikipedia was launched in 2010
+             As of June 2020, the Bengali Wikipedia is the only online free 
+            encyclopedia written in the Bengali language. It is also one of 
+            the largest Bengali content related sites on the internet. 
+            The mobile version of the Bengali Wikipedia was launched in 2010
+             
+              As of June 2020, the Bengali Wikipedia is the only online free 
+            encyclopedia written in the Bengali language. It is also one of 
+            the largest Bengali content related sites on the internet. 
+            The mobile version of the Bengali Wikipedia was launched in 2010 """
+    },
+
+    {
+        "slug":"hike-in-the-mountain",
+        "image": "mountains.jpeg",
+        "author": "Manoj",
+        "date": date(2022, 7, 7),
+        "title":"Mountain Hiking",
+        "excerpt": "There's is nothing like views you get when hiking in the mountains!I wasn't even prepared for what happend whilst I was enjoying the view!",
+        "content": """
+            As of June 2020, the Bengali Wikipedia is the only online free 
+            encyclopedia written in the Bengali language. It is also one of 
+            the largest Bengali content related sites on the internet. 
+            The mobile version of the Bengali Wikipedia was launched in 2010
+             As of June 2020, the Bengali Wikipedia is the only online free 
+            encyclopedia written in the Bengali language. It is also one of 
+            the largest Bengali content related sites on the internet. 
+            The mobile version of the Bengali Wikipedia was launched in 2010
+             
+              As of June 2020, the Bengali Wikipedia is the only online free 
+            encyclopedia written in the Bengali language. It is also one of 
+            the largest Bengali content related sites on the internet. 
+            The mobile version of the Bengali Wikipedia was launched in 2010
+             As of June 2020, the Bengali Wikipedia is the only online free 
+            encyclopedia written in the Bengali language. It is also one of 
+            the largest Bengali content related sites on the internet. 
+            The mobile version of the Bengali Wikipedia was launched in 2010
+             
+              As of June 2020, the Bengali Wikipedia is the only online free 
+            encyclopedia written in the Bengali language. It is also one of 
+            the largest Bengali content related sites on the internet. 
+            The mobile version of the Bengali Wikipedia was launched in 2010 """
+    }
+
+
+
+]
 
 # Create your views here.
 
