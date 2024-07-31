3eb594945f042bcc98a8af17fd56f3636d126d81
Manoj-Ghosh <manojghoshdk@gmail.com>
2024-07-31 20:24:03 +0530

    multiple changes happens in models.py and admin.py

--- My_Site/blog/admin.py
+++ My_Site/blog/admin.py
@@ -4,4 +4,8 @@ from django.contrib import admin
 # Register your models here.
 
 
+from .models import Post, Author, Tag
 
+admin.site.register(Post)
+admin.site.register(Author)
+admin.site.register(Tag)
